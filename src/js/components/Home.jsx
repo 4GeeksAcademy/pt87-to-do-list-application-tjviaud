ec6e8fae8b8e2d740a352f81978c993b5948a430
TJ Viaud <tjviaud@gmail.com>
2025-12-16 22:40:02 +0000

    save

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,16 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
   
+  const loadUser=async() => {
+    const response=await fetch ("https://playground.4geeks.com/todo/users/tj")
+    if (!response.ok) {
+      const response=await fetch ("https://playground.4geeks.com/todo/users/tj",{method:"post"})
+      return 
+    }
+    const data=await response.json()
+    setTask(data.todos)
+  }
+  useEffect(()=>{loadUser()},[])
+  const addTask=async(event)=>{
+    if (event.key ==="Enter"&& newTask.trim()!=="") {
+      const newTaskObject= {label:newTask,is_done:false};
 
-  const visibleTodos=todos.slice(-5);
-  const hiddenCount=Math.max(todos.length - visibleTodos.length, 0);
+      const response=await fetch("https://playground.4geeks.com/todo/todos/tj", {
+        method: "post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newTaskObject),
+      });
+      const data=await response.json ();
+      setNewTask([...task,data]);
+      setNewTask("");
+    }
+  };
+  const deleteTask=async(taskId)=>{
+    try{await fetch(`https://playground.4geeks.com/todo/todos/${taskId}`,
+      {method:"delete"}
+    );
+    setTask(task.filter((task)=> taskId !== task.id));
+    console.log(`task with id ${taskId} deleted`);
+  }catch (error) {console.error("error deleting task", error);}
+  };
+
+  
   return (
     <div className="container">
       <h1>My ToDo List</h1>
@@ -18,12 +49,12 @@ const Home = () => {
         <li>
           <input
             type="text"
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange={(e) => setTask(e.target.value)}
             value={inputValue}
             onKeyPress={(e) => {
               if (e.key === "Enter") {
-                setTodos(todos.concat(inputValue));
-                setInputValue("");
+                setNewTask(todos.concat(inputValue));
+                setTask("");
               }
             }}
             placeholder="Add a task"
@@ -37,7 +68,7 @@ const Home = () => {
             <i
               class="fas fa-trash-alt"
               onClick={() =>
-                setTodos(
+                setNewTask(
                   todos.filter((t, currentIndex) => index != currentIndex)
                 )
               }
